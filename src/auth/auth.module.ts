266082f97332cca91af4c3732b3206b52a8a6c50
Ivan Zorin <creator@localzet.com>
2025-04-03 16:24:49 +0300

    Update JWT

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,16 +1,21 @@
-import { JwtModule } from '@nestjs/jwt';
+import {JwtModule} from '@nestjs/jwt';
 import {AuthService} from "./auth.service";
 import {AuthController} from "./auth.controller";
 import {Module} from "@nestjs/common";
+import {PassportModule} from "@nestjs/passport";
+import {JwtStrategy} from "./jwt.strategy";
 
 @Module({
-  imports: [
-    JwtModule.register({
-      secret: 'test-secret-key',
-      signOptions: { expiresIn: '30d' },
-    }),
-  ],
-  providers: [AuthService],
+    imports: [
+        PassportModule.register({defaultStrategy: 'jwt'}),
+        JwtModule.register({
+            secret: 'test-secret-key',
+            signOptions: {expiresIn: '30d'},
+        }),
+    ],
+  providers: [AuthService, JwtStrategy],
   controllers: [AuthController],
+  exports: [AuthService, JwtStrategy],
 })
-export class AuthModule {}
+export class AuthModule {
+}
