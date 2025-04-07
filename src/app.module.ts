455f77dc72aaaf4f1a13024b2467f5fa4b2cae04
Ivan Zorin <i.zorin@geryon.space>
2025-04-07 14:39:05 +0300

    Upd

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
+import {Module} from '@nestjs/common';
+import {PrismaModule} from './prisma/prisma.module';
+import {AuthModule} from './auth/auth.module';
+import {UsersModule} from './users/users.module';
+import {TasksModule} from "./tasks/tasks.module";
+import {ReportsModule} from "./reports/reports.module";
+import {TimerModule} from "./timer/timer.module";
 
 @Module({
-  imports: [PrismaModule, AuthModule, UsersModule],
+    imports: [PrismaModule, AuthModule, ReportsModule, TasksModule, TimerModule, UsersModule],
 })
-export class AppModule {}
+export class AppModule {
+}
